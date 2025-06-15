4425629d4e9f39acf16f514cb69403c852568006
angl-ortixz <cruzzitoxd12@gmail.com>
2025-06-15 00:06:02 -0600

    Update inicio.php

--- inicio.php
+++ inicio.php
@@ -28,7 +28,7 @@
       <div class="menu">
         <nav>
           <ul>
-            <li><a href="index.php" id="selected">Inicio</a></li>
+            <li><a href="index.html" id="selected">Inicio</a></li>
             <li><a href="zonascul.php">Lugares Culturales</a></li>
             <li><a href="zonasdepor.php">Lugares Deportivos</a></li>
             <li><a href="zonasturi.php">Lugares Turísticos</a></li>
